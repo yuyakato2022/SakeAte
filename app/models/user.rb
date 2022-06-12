4ddbe6bf4a324e5a00cc05295f87421aa1336d01
yuyakato2022 <katoyuya.sevic1@outlook.jp>
2022-06-12 12:54:46 +0000

    ゲストログイン機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :bookmarks, dependent: :destroy
   has_many :recipes, through: :bookmarks, dependent: :destroy
-  
+
   has_many :reviews, dependent: :destroy
-   
+
   has_one_attached :profile_image
+
+  def self.guest
+   
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = 'ゲスト'
+    end
+  end
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
     sessions: 'public/sessions'
   }
 
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+
   root to: 'public/recipes#top'
 
   namespace :admin_user do
