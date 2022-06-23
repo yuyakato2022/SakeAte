f64f5596064ef6456eafa056b7389a9b1eabbac6
yuyakato2022 <katoyuya.sevic1@outlook.jp>
2022-06-23 04:19:38 +0000

    修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Admin_user.cerate!(
+AdminUser.create!(
   email: 'hoge@g',
   password: 'HogeHogeHoge',
 )
